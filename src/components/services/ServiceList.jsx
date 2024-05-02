ac03fc22c8ba35262399451a88f8e5b926f27f82
anickacodes <anickalewis@pursuit.org>
2024-05-01 22:05:29 -0400

    updated json for current styles + ux/ui

--- src/components/services/ServiceList.jsx
+++ src/components/services/ServiceList.jsx
@@ -1,32 +1,56 @@
-import React from "react";
-import hairBraidingServicesData from "./ServiceInfo.json";
+import React, { useState } from "react";
+import serviceData from "./ServiceInfo.json";
 import { Link } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Collapse, Card, Button } from "react-bootstrap";
 import "./ServiceList.css"; 
 
 function ServiceList() {
-  const { hair_braiding_services } = hairBraidingServicesData;
+  const { service_categories } = serviceData;
+  const [openCategory, setOpenCategory] = useState(null);
+
+  const toggleCollapse = (category) => {
+    setOpenCategory(openCategory === category ? null : category);
+  };
 
   return (
-    <Container className="service-list-container text-center">
-      <div className="row">
-        <div className="col-sm">
-       
-            <ul>
-              {hair_braiding_services.map((service, index) => (
-                <li key={index} className="service-item">
-                  <Link to={service.name} className="service-name" aria-label={service.name}>{service.name}</Link>
-                <br />
-                <span className="service-description" aria-label={`Description: ${service.description}`}>Description: {service.description}</span>
-                <br />
-                <span className="service-price" aria-label={`Price: $${service.price}`}>Price: ${service.price}</span>
-              </li>
-              ))}
-            </ul>
-          </div>
-     
-      </div>
-    </Container>
+    <div className="service-list-container text-center" >
+      {Object.entries(service_categories).map(([category, services], index) => (
+        <Card key={index} className="card-container m-1">
+          <Card.Header className="category-header">
+            <Button
+              onClick={() => toggleCollapse(category)}
+              aria-controls={`collapse-${category}`}
+              aria-expanded={openCategory === category}
+              variant="link"
+              className="category-toggle"
+            >
+              {category.toUpperCase()}
+            </Button>
+          </Card.Header>
+          <Collapse in={openCategory === category}>
+            <Card.Body id={`collapse-${category}`}>
+              <ul className="service-list">
+                {services.map((service, serviceIndex) => (
+                  <li key={serviceIndex} className="service-item">
+                    <Link to={`/services/${category}/${service.name}`} className="service-name" aria-label={service.name}>
+                      {service.name}
+                    </Link>
+                    <br />
+                    <span className="service-description" aria-label={`Description: ${service.description}`}>
+                      Description: {service.description}
+                    </span>
+                    <br />
+                    <span className="service-price" aria-label={`Price: $${service.price}`}>
+                      Price: ${service.price}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </Card.Body>
+          </Collapse>
+        </Card>
+      ))}
+    </div>
   );
 }
 
